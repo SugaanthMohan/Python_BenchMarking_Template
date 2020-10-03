d06181b639f272fb6afea4487e1ed761b225ef21
Suganth-DMac <sugaanth.mohan@gmail.com>
2020-10-04 05:24:14 +0530

    Added Secs Code for Standard Bench Marker
    Added code for calculating seconds in the benchmarking process to get a general idea on the total time the process took

---
+++ Utils/StandardBenchMarker.py
@@ -0,0 +1,41 @@
+# IMPORT STATEMENTS HERE
+# FUNC TOOLS DECORATOR/WRAPPER
+from functools import wraps
+# TIME MODULE FOR SECONDS CALCULATION
+import time
+
+
+class Utils:
+    """
+    Set of Coding Utilities for python.
+    """
+
+    def __init__(self):
+        """
+        INTIALIZE
+        """
+        pass
+
+    def benchmark_sec(self, function_wrapper):
+        """
+        Main functional wrapper code for function that will be used for further calling inbound functions
+        :param function_wrapper: the function and args wrapped into one
+        :return: wrap
+        """
+
+        # SET THE WRAPPER HERE
+        @wraps(function_wrapper)
+        def wrap(*args, **kwargs):
+            """
+            The inner wrapper function that will handle execution of the function
+            :param args: The Positional arguments list.
+            :param kwargs: The keyword arugments list.
+            :return:
+            """
+            start_time = time.time()
+            result = function_wrapper(*args, **kwargs)
+            print("Function : %r | Args: (Positional : %r, Keyword : %r) | Elapsed Secs: %2.3f sec(s)" %
+                  (function_wrapper.__name__, args, kwargs, time.time() - start_time)
+                  )
+            return result
+        return wrap
